79821943665f5b98a33ed18912588f729067f5a0
Ubuntu <ubuntu@ip-172-26-3-220.us-west-2.compute.internal>
2018-05-22 16:02:50 +0000

    Integrated filepreview with the express server, form requests are handled and process in this version and a jpg output is given as the response.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
-var filepreview = require('filepreview');
+const express = require('express');
+const bodyParser = require('body-parser');
+const formidable = require('formidable');
+const filepreview = require('filepreview');
+const util = require('util');
 
-let fileIn = process.argv[2];
+const app = express();
 
-if (fileIn) {
-  let fileOut = fileIn.split('.')[0] + '.jpg';
-  filepreview.generate(fileIn, fileOut, function(error) {
-    if (error) {
-      return console.log(error);
-    } else {
-      console.log('File preview completed');
-    }
-  });
-} else {
-  console.log('You need to pass a file as the argument.');
-}
+//app.use(express.static('public'))
+
+app.get('/', (req, res) => { res.send('Get request from Express!') })
+app.post('/', (req, res) => { res.send('Post request from Express!') })
+
+app.post('/preview/url', (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req)
+
+  form.on('fileBegin', (name, file) => {
+    file.path = __dirname + '/uploads/' + file.name
+  })
+
+  form.on('file', (name, file) => {
+    let fileOut = __dirname + '/uploads/' + file.name.split('.')[0] + '.jpg';
+    let options = { quality: 100, pagerage: '1-100' }
+    filepreview.generate( __dirname + '/uploads/' + file.name, fileOut, options, () => res.sendFile(fileOut));
+  })
+})
+
+app.post('/preview/file', (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.on('fileBegin', (name, file) => {
+    file.path = __dirname + '/uploads/' + file.name
+  })
+
+  form.on('file', (name, file) => {
+    let fileOut = __dirname + '/uploads/' + file.name.split('.')[0] + '.jpg';
+    let options = { quality: 100, pagerage: '1-100' }
+    filepreview.generate( __dirname + '/uploads/' + file.name, fileOut, options, () => res.sendFile(fileOut));
+  })
+})
+
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, can't find that!")
+})
+
+app.listen(3000, () => { console.log('Server is listening on port 3000') })
+
+
